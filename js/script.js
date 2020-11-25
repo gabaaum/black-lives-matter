571a5783725e92d811fc3141c9bf28399a427537
gabaaum <gabriel.mariano12@hotmail.com>
2020-11-24 21:31:08 -0300

    black lives matter

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$("body").on('click', '.icone_exibir', function(event) {
+		event.preventDefault();
+		$(".barra_lateral").toggleClass("exibir_conteudo");
+		$(".icone_exibir").css('display', 'none');
+		$(".icone_ocultar").css('display', 'block');
+	});
+
+	$("body").on('click', '.icone_ocultar', function(event) {
+		event.preventDefault();
+		$(".barra_lateral").toggleClass("exibir_conteudo");
+		$(".icone_exibir").css('display', 'block');
+		$(".icone_ocultar").css('display', 'none');
+	});
+});
